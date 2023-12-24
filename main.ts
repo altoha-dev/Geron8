c749599455cf7940197421cd15a40250429ea18d
Altynbek <you@example.com>
2023-12-24 22:27:25 +0600

    Trello9 Chat

--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
-import *as T1 from "./src/scripts/TTask1"
-import *as T2 from "./src/scripts/TTask2"
-import *as T3 from "./src/scripts/TTask3"
+import *as T1 from "./src/scripts/Trello8.1"
+import *as T2 from "./src/scripts/Trello8.2"
+import *as T3 from "./src/scripts/Trello8.3"
+import *as Trello9 from "./src/scripts/Trello9"
+//8.1
 //console.log(T1.maxIncomeSeason)
 //console.log(T1.minIncomeSeason)
 //console.log(T1.maxDismissedSeason)
 //console.log(T1.maxHiredSeason)
 
-
+//8.2
 //console.log(T2.closedProjectsEmployees)
 //console.log(`большая средняя ЗП У= ${T2.departmentWithMaxAverageSalary}`)
 //console.log(T2.maxExperienceDepartment)
 
-console.log(T3.sortedByExperience)
-console.log(` отдел в которм сотрудники проработали дольше всего= ${T3.departmentWithLongestWorkPeriod}`)
-console.log(`среднюю зарплату сотрудников уволенных по собственному желанию ${T3.averageSalaryForResigned}`)
+//8.3
+//console.log(T3.sortedByExperience)
+//console.log(` отдел в которм сотрудники проработали дольше всего= ${T3.departmentWithLongestWorkPeriod}`)
+//console.log(`среднюю зарплату сотрудников уволенных по собственному желанию ${T3.averageSalaryForResigned}`)
+
+//9
+
+console.log(Trello9.calculateDepartmentBudget)
+
+
+
+
 
